d7fefa1e4b4c9b5fbe90abe19835c4aef85d20e1
iwanhkim <ikhwanulhkim@gmail.com>
2023-12-07 22:55:47 +0800

    setup fragment for main activity

--- app/src/main/java/com/puitika/ui/main/MainActivity.kt
+++ app/src/main/java/com/puitika/ui/main/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.puitika.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 import com.puitika.R
 import com.puitika.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,16 +17,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         with(binding) {
-            btmNav.add(MeowBottomNavigation.Model(1, R.drawable.event_ic))
-            btmNav.add(MeowBottomNavigation.Model(2, R.drawable.scan_ic))
-            btmNav.add(MeowBottomNavigation.Model(3, R.drawable.home_ic))
-            btmNav.show(1)
-        }
-        navigation()
+            bottomNav.add(MeowBottomNavigation.Model(1, R.drawable.home_ic))
+            bottomNav.add(MeowBottomNavigation.Model(2, R.drawable.scan_ic))
+            bottomNav.add(MeowBottomNavigation.Model(3, R.drawable.event_ic))
+            bottomNav.show(1)
+            navigation(HomeFragment())
 
+            bottomNav.setOnClickMenuListener {
+                when (it.id) {
+                    1 -> navigation(HomeFragment())
+                    2 -> navigation(ScanFragment())
+                    3 -> navigation(EventFragment())
+                }
+            }
+        }
     }
 
-    private fun navigation(){
-
+    private fun navigation(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container, fragment)
+        fragmentTransaction.commit()
     }
 }

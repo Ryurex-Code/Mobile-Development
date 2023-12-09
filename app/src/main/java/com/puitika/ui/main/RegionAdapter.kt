d1af1c88df42e483363e5ffae62798d71149fc71
iwanhkim <ikhwanulhkim@gmail.com>
2023-12-09 16:39:34 +0800

    designing region_list_item.xml and creating its adapter

---
+++ app/src/main/java/com/puitika/ui/main/RegionAdapter.kt
@@ -0,0 +1,49 @@
+package com.puitika.ui.main
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.puitika.R
+import com.puitika.data.dummy.DetailRegion
+
+class RegionAdapter(private val context: Context, private val regionList: List<DetailRegion>) :
+    RecyclerView.Adapter<RegionAdapter.ViewHolder>() {
+
+    private var onItemClickListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        onItemClickListener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.region_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Glide.with(context).load(regionList[position].imageURL).into(holder.imageView)
+        holder.tvRegion.text = regionList[position].name
+
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.onClick(holder.imageView, regionList[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return regionList.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.list_item_image)
+        val tvRegion: TextView = itemView.findViewById(R.id.tv_region)
+    }
+
+    interface OnItemClickListener {
+        fun onClick(imageView: ImageView, region: DetailRegion)
+    }
+}

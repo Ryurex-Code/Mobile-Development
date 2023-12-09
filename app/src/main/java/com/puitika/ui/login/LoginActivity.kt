029b3838527026672870a8e07b30ec2eea4e4e01
iwanhkim <ikhwanulhkim@gmail.com>
2023-12-09 16:40:22 +0800

    setup some action (temporary)

--- app/src/main/java/com/puitika/ui/login/LoginActivity.kt
+++ app/src/main/java/com/puitika/ui/login/LoginActivity.kt
@@ -1,12 +1,35 @@
 package com.puitika.ui.login
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.puitika.R
+import com.puitika.databinding.ActivityLoginBinding
+import com.puitika.databinding.ActivityMainBinding
+import com.puitika.ui.main.MainActivity
+import com.puitika.ui.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setAction()
+
+    }
+
+    private fun setAction(){
+        binding.btnLogin.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("fromLogin", true)
+            startActivity(intent)
+        }
+        binding.tvRegister.setOnClickListener{
+            startActivity(Intent(this,RegisterActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/puitika/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/puitika/ui/register/RegisterActivity.kt
@@ -3,10 +3,27 @@ package com.puitika.ui.register
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.puitika.R
+import com.puitika.databinding.ActivityLoginBinding
+import com.puitika.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setAction()
+    }
+
+    private fun setAction(){
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+        binding.btnConfirm.setOnClickListener {
+            finish()
+        }
     }
 }

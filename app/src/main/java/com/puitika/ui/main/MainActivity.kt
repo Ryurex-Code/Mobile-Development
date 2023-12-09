9c5d7945a4f3d4e28ea3002f5cd0ae5bf8406ae2
iwanhkim <ikhwanulhkim@gmail.com>
2023-12-09 16:38:08 +0800

    navigation logic (temporary)

--- app/src/main/java/com/puitika/ui/main/MainActivity.kt
+++ app/src/main/java/com/puitika/ui/main/MainActivity.kt
@@ -25,18 +25,51 @@ class MainActivity : AppCompatActivity() {
 
             bottomNav.setOnClickMenuListener {
                 when (it.id) {
-                    1 -> navigation(HomeFragment())
-                    2 -> navigation(ScanFragment())
-                    3 -> navigation(EventFragment())
+                    1 -> navigation(HomeFragment(),false)
+                    2 -> navigation(ScanFragment(),false)
+                    3 -> navigation(EventFragment(),false)
                 }
             }
         }
+
+        supportFragmentManager.addOnBackStackChangedListener(onBackStackChangedListener)
     }
 
-    private fun navigation(fragment: Fragment) {
+    override fun onDestroy() {
+        super.onDestroy()
+        supportFragmentManager.removeOnBackStackChangedListener(onBackStackChangedListener)
+    }
+
+    private val onBackStackChangedListener =
+        FragmentManager.OnBackStackChangedListener {
+            val currentFragment = supportFragmentManager.findFragmentById(R.id.container)
+            currentFragment?.let { updateBottomNavigation(it) }
+        }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            supportFragmentManager.popBackStack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    private fun navigation(fragment: Fragment, first:Boolean) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.container, fragment)
+        if (!first){
+        fragmentTransaction.addToBackStack(null)}
         fragmentTransaction.commit()
+        updateBottomNavigation(fragment)
     }
-}
+
+    private fun updateBottomNavigation(fragment: Fragment) {
+        val selectedItem = when (fragment) {
+            is HomeFragment -> 1
+            is ScanFragment -> 2
+            is EventFragment -> 3
+            else -> return
+        }
+        binding.bottomNav.show(selectedItem, true)
+    }

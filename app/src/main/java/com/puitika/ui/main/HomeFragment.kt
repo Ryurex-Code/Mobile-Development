7acf8ffb72b2f30626846991b245898a03e337b3
iwanhkim <ikhwanulhkim@gmail.com>
2023-12-10 02:20:15 +0800

    add several import

--- app/src/main/java/com/puitika/ui/main/HomeFragment.kt
+++ app/src/main/java/com/puitika/ui/main/HomeFragment.kt
@@ -1,19 +1,23 @@
 package com.puitika.ui.main
 
-import android.content.Intent
-import android.os.Bundle
-import android.os.Handler
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.puitika.R
-import com.puitika.data.dummy.dummyTraditionalCloths
-import com.puitika.data.dummy.regionList
-import com.puitika.databinding.FragmentHomeBinding
-import com.puitika.ui.profile.ProfileActivity
+    import android.content.Intent
+    import android.os.Bundle
+    import android.os.Handler
+    import android.util.Log
+    import androidx.fragment.app.Fragment
+    import android.view.LayoutInflater
+    import android.view.View
+    import android.view.ViewGroup
+    import android.widget.ImageView
+    import androidx.recyclerview.widget.LinearLayoutManager
+    import androidx.recyclerview.widget.StaggeredGridLayoutManager
+    import com.puitika.R
+    import com.puitika.data.dummy.DetailRegion
+    import com.puitika.data.dummy.dummyTraditionalCloths
+    import com.puitika.data.dummy.regionList
+    import com.puitika.databinding.FragmentHomeBinding
+    import com.puitika.ui.detail.DetailDaerahActivity
+    import com.puitika.ui.profile.ProfileActivity
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -56,13 +60,13 @@ class HomeFragment : Fragment() {
                 } else {
                     binding.rvRegion.smoothScrollToPosition(0)
                 }
-                handler.postDelayed(this, 3000) // Adjust the delay as needed (in milliseconds)
+                handler.postDelayed(this, 3000)
             }
         }
+        handler.postDelayed(runnable, 3000)
 
-        handler.postDelayed(runnable, 3000) // Initial delay before the auto-scroll starts
 
-        val clothAdapter = ClothAdapter(requireContext(), dummyTraditionalCloths.data)
+        val clothAdapter = ClothesAdapter(requireContext(), dummyTraditionalCloths.data)
         val spanCount = 2
         binding.rvCloth.apply {
             layoutManager = StaggeredGridLayoutManager(spanCount, StaggeredGridLayoutManager.VERTICAL)

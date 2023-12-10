b46576309c93f4acabff2ddcd300ac66cd6a4d29
iwanhkim <ikhwanulhkim@gmail.com>
2023-12-10 14:47:21 +0800

    class for injecting repository

--- app/src/main/java/com/puitika/di/Injection.kt
+++ app/src/main/java/com/puitika/di/Injection.kt
@@ -1,5 +1,14 @@
 package com.puitika.di
 
-object Injection {
+import android.content.Context
+import com.puitika.data.local.AccountPreference
+import com.puitika.data.remote.network.ApiConfig
+import com.puitika.repository.PuitikaRepository
 
+object Injection {
+    fun provideRepository(context: Context): PuitikaRepository {
+        val preferences = AccountPreference(context)
+        val apiService = ApiConfig.getApiService()
+        return PuitikaRepository.getInstance(preferences, apiService)
+    }
 }

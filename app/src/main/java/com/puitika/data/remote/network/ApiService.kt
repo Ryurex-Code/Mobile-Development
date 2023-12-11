b20a127c1464680ae43bad5d1c5ac034888ff453
iwanhkim <ikhwanulhkim@gmail.com>
2023-12-12 01:28:39 +0800

    import for ApiService.kt

--- app/src/main/java/com/puitika/data/remote/network/ApiService.kt
+++ app/src/main/java/com/puitika/data/remote/network/ApiService.kt
@@ -1,5 +1,10 @@
 package com.puitika.data.remote.network
 
+import com.puitika.data.request.RegisterRequest
+import com.puitika.data.remote.response.RegisterResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 
 interface ApiService {
     @POST("/register")

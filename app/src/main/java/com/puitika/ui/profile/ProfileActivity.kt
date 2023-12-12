b1817a2d8cee684c1c1226609ce7d4b21985b18a
nihtlouw <hirumiminazuki360@gmail.com>
2023-12-12 18:01:41 +0700

    adding setting goes to languange setting on mobile android

--- app/src/main/java/com/puitika/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/puitika/ui/profile/ProfileActivity.kt
@@ -1,32 +1,67 @@
 package com.puitika.ui.profile
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.puitika.R
 import com.puitika.ui.login.LoginActivity
+import com.puitika.ui.profile.ProfileEditActivity
 
 class ProfileActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-        // Find the TextView by its ID
+        // Find the RelativeLayout and TextView by their IDs
+        val icWorldIcon = findViewById<RelativeLayout>(R.id.ic_world_icon)
+        val tvChangeLang = findViewById<TextView>(R.id.tv_change_lang)
+
+        // Set an OnClickListener for the RelativeLayout
+        icWorldIcon.setOnClickListener {
+            // Handle click on the RelativeLayout
+            openLanguageSettings()
+        }
+
+        // Set an OnClickListener for the TextView
+        tvChangeLang.setOnClickListener {
+            // Handle click on the TextView
+            openLanguageSettings()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_profile, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_edit -> {
+                val intent = Intent(this, ProfileEditActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun logout() {
         val tvLogOut = findViewById<TextView>(R.id.tv_log_out)
-        val rectangle5 = findViewById<View>(R.id.rectangle_5)
-
-        // Set an OnClickListener on the TextView
         tvLogOut.setOnClickListener {
-            // Create an Intent to start the LoginActivity
             val intent = Intent(this, LoginActivity::class.java)
-
-
-            // You can add any additional logic or data passing here if needed
-
-            // Start the LoginActivity
             startActivity(intent)
         }
+    }
+
+    private fun openLanguageSettings() {
+        // Intent untuk membuka pengaturan bahasa di aplikasi Pengaturan (Settings)
+        val languageIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+        startActivity(languageIntent)
     }
 }

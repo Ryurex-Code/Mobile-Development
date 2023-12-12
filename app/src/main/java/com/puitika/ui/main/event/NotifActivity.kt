bd7f340f7eba69ddb789353305d6f1a3d8ba3688
nihtlouw <hirumiminazuki360@gmail.com>
2023-12-12 18:04:04 +0700

    add notif act and its xml

---
+++ app/src/main/java/com/puitika/ui/main/event/NotifActivity.kt
@@ -0,0 +1,12 @@
+package com.puitika.ui.main.event
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.puitika.R
+
+class NotifActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notif)
+    }
+}

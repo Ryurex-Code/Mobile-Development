84024bf2b83b1b8faae8b72ed55192ae54d6ca81
nihtlouw <hirumiminazuki360@gmail.com>
2023-12-14 21:54:06 +0700

    fixinf scroll view for text view in detail

--- app/src/main/java/com/puitika/ui/main/event/EventDetailActivity.kt
+++ app/src/main/java/com/puitika/ui/main/event/EventDetailActivity.kt
@@ -3,12 +3,9 @@ package com.puitika.ui.main.event
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.puitika.data.dummy.DetailEvent
-import com.puitika.data.dummy.Event
 import com.puitika.databinding.ActivityEventDetailBinding
-import com.puitika.ui.detail.region_detail.ViewMoreAdapter
 
 class EventDetailActivity : AppCompatActivity() {
 
@@ -19,7 +16,6 @@ class EventDetailActivity : AppCompatActivity() {
         binding = ActivityEventDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Get the event ID from the intent
         val event = if (Build.VERSION.SDK_INT >= 33) {
             intent.getParcelableExtra("EXTRA_EVENT", DetailEvent::class.java)
         } else {
@@ -29,30 +25,18 @@ class EventDetailActivity : AppCompatActivity() {
 
         if (event != null) {
             setupView(event)
-            setupRecycleView(event)
         }
     }
 
     private fun setupView(event: DetailEvent) {
-        Glide.with(this).load(event.BannerURL).into(binding.ivImageView2)
+        Glide.with(this).load(event.gambar).into(binding.ivImageview2)
         binding.apply {
-            tvEventname.text = event.Name
-            tvEventtime.text = event.Time
-            tvEventdate.text = event.Date
-            tvLocation.text = event.Location
-            tvDetailEvent.text = event.Description
-        }
-    }
-
-    private fun setupRecycleView(detailEvents: List<DetailEvent>) {
-        val eventAdapter = ViewMoreAdapter(this, detailEvents)
-        binding.tvDetailEvent.apply {
-            layoutManager = LinearLayoutManager(
-                this@EventDetailActivity,
-                LinearLayoutManager.VERTICAL,
-                false
-            )
-            adapter = eventAdapter
+            tvEventname.text = event.nama
+            tvEventtimestart.text = event.mulai
+            tvEventtimeend.text = event.selesai
+            tvEventdate.text = event.waktu
+            tvLocation.text = event.lokasi
+            tvDetailEvent.text = event.description
         }
     }
 }

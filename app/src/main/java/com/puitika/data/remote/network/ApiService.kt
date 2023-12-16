7cae819fd2adb26657fb35d0c85652b75e813d14
iwanhkim <ikhwanulhkim@gmail.com>
2023-12-16 23:54:17 +0800

    add several import

--- app/src/main/java/com/puitika/data/remote/network/ApiService.kt
+++ app/src/main/java/com/puitika/data/remote/network/ApiService.kt
@@ -1,10 +1,13 @@
 package com.puitika.data.remote.network
 
+import com.puitika.data.remote.response.ClothResponse
 import com.puitika.data.remote.response.LoginResponse
+import com.puitika.data.remote.response.RegionResponse
 import com.puitika.data.request.RegisterRequest
 import com.puitika.data.remote.response.RegisterResponse
 import com.puitika.data.request.LoginRequest
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 

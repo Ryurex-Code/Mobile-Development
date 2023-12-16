ffa91f640983c84323142e6556f83e91c150e84e
iwanhkim <ikhwanulhkim@gmail.com>
2023-12-16 23:54:46 +0800

    import ClothDetail

--- app/src/main/java/com/puitika/ui/main/home/ClothesAdapter.kt
+++ app/src/main/java/com/puitika/ui/main/home/ClothesAdapter.kt
@@ -9,7 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.puitika.R
-import com.puitika.data.dummy.DetailCloth
+import com.puitika.data.remote.response.ClothDetail
 
 class ClothesAdapter(private val context: Context, private val cloth:List<DetailCloth>): RecyclerView.Adapter<ClothesAdapter.ViewHolder>() {
     private var onItemClickListener: OnItemClickListener? = null

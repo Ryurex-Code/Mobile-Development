e4bea9add1bb3ff745519b4d463d9851b2dc03e7
nihtlouw <hirumiminazuki360@gmail.com>
2023-12-17 14:06:26 +0700

    adding progress bar

--- app/src/main/java/com/puitika/ui/detail/cloth_detail/ClothDetailActivity.kt
+++ app/src/main/java/com/puitika/ui/detail/cloth_detail/ClothDetailActivity.kt
@@ -3,18 +3,31 @@ package com.puitika.ui.detail.cloth_detail
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.ProgressBar
 import com.bumptech.glide.Glide
+import com.puitika.R
 import com.puitika.data.dummy.DetailCloth
 import com.puitika.databinding.ActivityClothDetailBinding
 
 class ClothDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityClothDetailBinding
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityClothDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        progressBar = findViewById(R.id.progress_bar)
+        progressBar.visibility = View.VISIBLE
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Hide the progress bar once the data is loaded
+            progressBar.visibility = View.GONE
+        }, 1000)
+
 
         val cloth = if (Build.VERSION.SDK_INT >= 33) {
             intent.getParcelableExtra("EXTRA_CLOTH", DetailCloth::class.java)

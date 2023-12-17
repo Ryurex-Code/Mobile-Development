00b61d76fb8b624e69611e3ee9ef764563f919be
nihtlouw <hirumiminazuki360@gmail.com>
2023-12-17 13:17:38 +0700

    adding pop up acc succes registered and progress bar

--- app/src/main/java/com/puitika/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/puitika/ui/register/RegisterActivity.kt
@@ -1,18 +1,34 @@
 package com.puitika.ui.register
 
+import android.app.Dialog
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Gravity
+import android.view.View
+import android.view.Window
+import android.view.inputmethod.InputMethodManager
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import com.puitika.utils.showToast
 import androidx.activity.viewModels
+import com.google.android.material.internal.ViewUtils.hideKeyboard
+import com.puitika.R
 import com.puitika.data.model.RegisterModel
 import com.puitika.databinding.ActivityRegisterBinding
 import com.puitika.factory.ViewModelFactory
+import com.puitika.ui.login.LoginActivity
 import com.puitika.utils.Result
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
     private lateinit var factory: ViewModelFactory
+    private lateinit var progressBar: ProgressBar
     private val viewModel: RegisterViewModel by viewModels { factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,21 +36,27 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setViewModelFactory()
+        progressBar = findViewById(R.id.progress_bar)
         setAction()
     }
 
     private fun setAction() {
         binding.btnBack.setOnClickListener {
-            finish()
+            progressBar.visibility = View.VISIBLE
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                progressBar.visibility = View.GONE
+                finish()
+            }, 1000)
         }
 
         binding.btnConfirm.setOnClickListener {
+
             val username = binding.etUsername.text.toString()
             val email = binding.etEmail.text.toString()
             val password = binding.etPassword.text.toString()
             val repassword = binding.etRePassword.text.toString()
 
-            // Clear previous error messages
             binding.etUsername.error = null
             binding.etEmail.error = null
             binding.etPassword.error = null
@@ -83,11 +105,18 @@ class RegisterActivity : AppCompatActivity() {
                         is Result.Loading -> {}
                         is Result.Error -> {
                             showToast(this, result.data)
+                            progressBar.visibility = View.GONE
                         }
 
                         is Result.Success -> {
-                            showToast(this, result.data.message)
-                            finish()
+                            progressBar.visibility = View.GONE
+                            showSuccessDialog()
+
+                            Handler(Looper.getMainLooper()).postDelayed({
+                                if (!isFinishing) {
+                                    startActivity(Intent(this, LoginActivity::class.java))
+                                }
+                            }, 2000)
                         }
                     }
                 }
@@ -98,5 +127,23 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun setViewModelFactory() {
         factory = ViewModelFactory.getInstance(binding.root.context)
+    }
+    private fun showSuccessDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.fragment_popup_acccreated)
+
+        val window = dialog.window
+        val layoutParams = window?.attributes
+        layoutParams?.gravity = Gravity.CENTER_VERTICAL
+        window?.attributes = layoutParams
+
+        dialog.show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            dialog.dismiss()
+            finish()
+        }, 1000)
     }
 }

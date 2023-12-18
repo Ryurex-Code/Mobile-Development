d2ea63181acb14dfe1bdeb9c83915aee5636ae65
iwanhkim <ikhwanulhkim@gmail.com>
2023-12-18 11:18:52 +0800

    recreate register logic

--- app/src/main/java/com/puitika/ui/register/RegisterViewModel.kt
+++ app/src/main/java/com/puitika/ui/register/RegisterViewModel.kt
@@ -11,21 +11,82 @@ import com.puitika.utils.Result
 
 class RegisterViewModel(private val repository: PuitikaRepository) : ViewModel() {
 
-    fun register(registerModel: RegisterModel): LiveData<Result<RegisterResponse>> {
-        val resultLiveData = MutableLiveData<Result<RegisterResponse>>()
+    private val resultLiveData = MutableLiveData<Result<RegisterResponse>>()
 
-        if (registerModel.password == registerModel.repassword) {
-            repository.register(RegisterRequest(
-                username = registerModel.username,
-                email = registerModel.email,
-                password = registerModel.password
-            )).observeForever { result ->
-                resultLiveData.value = result
-            }
+    fun register(registerModel: RegisterModel): LiveData<Result<RegisterResponse>> {
+        if (registerModel.username.isEmpty() || registerModel.email.isEmpty() || registerModel.password.isEmpty() || registerModel.repassword.isEmpty()) {
+            resultLiveData.value = Result.Error("All field should be filled!")
         } else {
-            resultLiveData.value = Result.Error("Password dan Konfirmasi Password tidak cocok")
+            val usernameValid = isUsernameValid(registerModel.username)
+            if (usernameValid) {
+                val emailValid = isEmailValid(registerModel.email)
+                if (emailValid) {
+                    val confirmPasswordValid = isConfirmPasswordValid(registerModel.password, registerModel.repassword)
+                    val passwordValid = isPasswordValid(registerModel.password)
+                    if (passwordValid) {
+                        if (confirmPasswordValid) {
+                            repository.register(
+                                RegisterRequest(
+                                    username = registerModel.username,
+                                    email = registerModel.email,
+                                    password = registerModel.password
+                                )
+                            ).observeForever { result ->
+                                resultLiveData.value = result
+                            }
+                        }
+                    }
+                }
+            }
         }
 
+
+
         return resultLiveData
+    }
+
+    private fun isUsernameValid(username: String): Boolean {
+        return if (username.isEmpty()) {
+            resultLiveData.value = Result.Error("Username is required")
+            false
+        } else if (username.contains(" ")) {
+            resultLiveData.value = Result.Error("Username should not contain spaces")
+            false
+        } else if (username.length < 5) {
+            resultLiveData.value = Result.Error("Username should be at least 5 characters long")
+            false
+        }else {
+            true
+        }
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        return if (email.isEmpty()) {
+            resultLiveData.value = Result.Error("Email is required")
+            false
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            resultLiveData.value = Result.Error("Invalid email format")
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        return if (password.isEmpty() || password.length < 8) {
+            resultLiveData.value = Result.Error("Password should be at least 8 characters long")
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun isConfirmPasswordValid(password: String, confirmPassword: String): Boolean {
+        return if (confirmPassword.isEmpty() || password != confirmPassword) {
+            resultLiveData.value = Result.Error("Passwords do not match")
+            false
+        } else {
+            true
+        }
     }
 }

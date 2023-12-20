34c1311179ff75ed256fa3e086de407c83bd0e7c
iwanhkim <ikhwanulhkim@gmail.com>
2023-12-21 00:28:04 +0800

    add scanCloth func

--- app/src/main/java/com/puitika/ui/main/scan/ScanViewModel.kt
+++ app/src/main/java/com/puitika/ui/main/scan/ScanViewModel.kt
@@ -2,7 +2,8 @@ package com.puitika.ui.main.scan
 
 import androidx.lifecycle.ViewModel
 import com.puitika.repository.PuitikaRepository
+import okhttp3.MultipartBody
 
 class ScanViewModel(private val repository:PuitikaRepository): ViewModel() {
-
+    fun scanCloth(file: MultipartBody.Part) = repository.scanCloth(file)
 }
